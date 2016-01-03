1e0c41ade555eae2d23b09157849aa5e6cb12836
Nick Webster <nick@nick.geek.nz>
2016-01-03 22:05:31 +1300

    Removing uneeded code

--- dashboard/edit.php
+++ dashboard/edit.php
@@ -246,9 +246,6 @@ foreach ($sql as $row) {
 	</div>
 	<!-- /#wrapper -->
 
-	<!-- jQuery -->
-	<script src="js/jquery.js"></script>
-
 	<!-- Bootstrap Core JavaScript -->
 	<script src="js/bootstrap.min.js"></script>
 
