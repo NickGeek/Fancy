6ba548f096227031daf14d499d9bf79db96fecf2
Nick Webster <nick@nick.geek.nz>
2016-01-05 16:55:00 +1300

    Adding updated FancyConnector to the demo site

--- demo/FancyConnector.php
+++ demo/FancyConnector.php
@@ -3,27 +3,44 @@ class FancyConnector {
 	protected $con;
 	protected $fancyVars;
 	public $preparedStatements = array();
+	public $site;
 
-	final public function __construct($site = false) {
+	public function __construct($site = false) {
 		//Get the user settings
 		include_once('settings.php');
 		$this->fancyVars = $fancyVars;
+		$this->site = $site;
 
 		//Connect to the database. In the future I might create a system to allow for this to work with custom connections
-		$this->con = new mysqli($fancyVars['dbaddr'], $fancyVars['dbuser'], $fancyVars['dbpass'], $fancyVars['dbname']);
+		$this->con = new mysqli($this->fancyVars['dbaddr'], $this->fancyVars['dbuser'], $this->fancyVars['dbpass'], $this->fancyVars['dbname']);
 		if ($this->con->connect_errno) { echo "<script>alert('Error connecting to Fancy database');</script>"; }
 		mysqli_set_charset($this->con, "utf8");
 
 		//Prepare common statements
-		if ($site) {
-			$this->preparedStatements['getElement'] = $this->con->prepare("/*".MYSQLND_QC_ENABLE_SWITCH."*/ SELECT `html` FROM `{$this->con->real_escape_string($site)}` WHERE `name` = ?;");
+		if ($this->fancyVars['apiVersion'] >= 2000) {
+			$this->preparedStatements['getElement'] = $this->con->prepare("/*".MYSQLND_QC_ENABLE_SWITCH."*/ SELECT `id`, `html` FROM `elements` WHERE `site` = ? AND `name` = ?;");
 		}
+		else {
+			//This is running on the old system
+			if ($this->site) $this->preparedStatements['getElement'] = $this->con->prepare("/*".MYSQLND_QC_ENABLE_SWITCH."*/ SELECT `id`, `html` FROM `{$this->con->real_escape_string($this->site)}` WHERE `name` = ?;");
+		}
+
+		$this->init();
+	}
+
+	protected function init() {
+		return;
 	}
 
 	public function fancy($name) {
-		$this->preparedStatements['getElement']->bind_param('s', $name);
+		if ($this->fancyVars['apiVersion'] >= 2000) {
+			$this->preparedStatements['getElement']->bind_param('ss', $this->site, $name);
+		}
+		else {
+			$this->preparedStatements['getElement']->bind_param('s', $name);
+		}
 		$this->preparedStatements['getElement']->execute();
-		$this->preparedStatements['getElement']->bind_result($element);
+		$this->preparedStatements['getElement']->bind_result($id, $element);
 		while ($this->preparedStatements['getElement']->fetch()) { echo '<div class="fancyElement" fancyname="'.$name.'">'.$element.'</div>'; }
 		return;
 	}
