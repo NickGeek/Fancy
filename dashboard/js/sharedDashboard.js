c2dde90fa0cafce9906a344b852da0d2ec3b20ae
Nick Webster <nick@nick.geek.nz>
2016-10-31 14:55:02 +1300

    Fixes #7

--- dashboard/js/sharedDashboard.js
+++ dashboard/js/sharedDashboard.js
@@ -11,6 +11,11 @@ function getType() {
 }
 
 function showSnackbar(message) {
+	if (get.id === '0') {
+		//Handle cases when this is a new element
+		window.location.href = "index.php?{0}={1}".format(getType(), getTitle());
+	}
+
 	$('#snackbar').html(message);
 	$('#snackbar').addClass('show');
 	setTimeout(function() { $('#snackbar').removeClass('show'); }, 1500);
